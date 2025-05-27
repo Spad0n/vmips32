6a05d851e1e1bd0d63b556452198b2d7c7d9e48f
Spad0n <92205208+Spad0n@users.noreply.github.com>
2025-05-27 15:53:04 +0200

    Adding argument for compiling and launch the program

--- nob.c
+++ nob.c
@@ -7,11 +7,14 @@ int main(int argc, char **argv) {
     Nob_Cmd cmd = {0};
 
     nob_cmd_append(&cmd, "gcc", "-ggdb", "-Wall", "-Wextra", "-o", "main", "src/mvm.c", "src/main.c", "src/utils.c", "src/lexer.c", "src/parser.c");
-    if (!nob_cmd_run_sync_and_reset(&cmd)) return 1;
-    //if (!nob_cmd_run_sync(cmd)) return 1;
 
-    nob_cmd_append(&cmd, "./main", "loop.mips");
-    nob_cmd_run_sync(cmd);
+    if (argc >= 2) {
+        if (!nob_cmd_run_sync_and_reset(&cmd)) return 1;
+        nob_cmd_append(&cmd, "./main", argv[1]);
+        nob_cmd_run_sync(cmd);
+    } else {
+        if (!nob_cmd_run_sync(cmd)) return 1;
+    }
 
     return 0;
 }
